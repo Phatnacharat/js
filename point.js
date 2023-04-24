bac5f8936d7a42158c4059e446696f69e60900f9
Phatnacharat <124545895+Phatnacharat@users.noreply.github.com>
2023-04-24 13:53:22 +0700

    Add files via upload

---
+++ point.js
@@ -0,0 +1,35 @@
+var tourpiont = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Bangkok",
+          "type": "culture",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.48216129283423,
+            13.740483913904853
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "samut Prakan",
+          "type": "park",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.55671796897127,
+            13.69949037631281
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
